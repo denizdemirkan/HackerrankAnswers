762362b6307c6aaf2fce666db973cde4de258375
denizdemirkan <deniz.demirkan@outlook.com.tr>
2023-03-09 14:22:46 +0300

    Init

---
+++ Cavity_Map/Program.cs
@@ -0,0 +1,31 @@
+﻿    List<string> grid = new List<string>();
+    grid.Add("1112");
+    grid.Add("1912");
+    grid.Add("1892");
+    grid.Add("1234");
+
+   //System.Console.WriteLine(grid.ElementAt(0).ElementAt(3));
+
+    cavityMap(grid);
+
+        static List<string> cavityMap(List<string> grid)
+    {
+        List<string> result = grid;
+
+        // ignore first and last row
+        for(int line=1; line<grid.Count-1; line++)
+        {
+            // ignore first and last cell
+            for(int cell=1; cell<grid.ElementAt(0).Length-1; cell++)
+            {
+                int a =  Convert.ToInt16(grid.ElementAt(line)[cell].ToString());
+                System.Console.WriteLine(a);
+            }
+        }
+        return result;
+    }
+
+// 1112
+// 1912
+// 1892
+// 1234
---
+++ Kaprekar_Number_Solve/Program.cs
@@ -0,0 +1,45 @@
+﻿kaprekarNumbers(1,99999);
+
+static void kaprekarNumbers(int p, int q)
+{
+    bool isAny = false;
+    for(long number = p; number<=q; number++)
+    {
+        int right;
+        int left;
+
+        string numString = (number*number).ToString();
+        
+        int howManyDigits = numString.Length;
+        if(howManyDigits%2 == 1)
+        {
+            right = howManyDigits/2+1;
+            left = howManyDigits/2;
+        }
+        else
+        {
+            right = howManyDigits/2;
+            left = howManyDigits/2;
+        }
+        
+        string leftPart = numString.Substring(0,left);
+        string rightPart = numString.Substring(left);
+
+
+        if(leftPart==""){
+            leftPart="0";
+        }
+        if(rightPart==""){
+            rightPart="0";
+        }
+        if(Convert.ToInt64(rightPart)+Convert.ToInt64(leftPart) == number)
+        {
+            isAny = true;
+            Console.Write(number+ " ");
+        }
+    }
+    if(!isAny)
+    {
+        Console.WriteLine("INVALID RANGE");
+    }
+}
